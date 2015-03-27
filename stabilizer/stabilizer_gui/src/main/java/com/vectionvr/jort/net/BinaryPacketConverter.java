b75d6cb1262d43e4973bf3dbd801476e76bdf830
Nico <nico@MacBook-Pro-de-Nicolas.local>
2015-03-27 22:32:18 +0100

    Added license file and headers

--- stabilizer/stabilizer_gui/src/main/java/com/vectionvr/jort/net/BinaryPacketConverter.java
+++ stabilizer/stabilizer_gui/src/main/java/com/vectionvr/jort/net/BinaryPacketConverter.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2014 Bnome SPRL (info@bnome.be)
+ *
+ * This file is part of VectionVR Stabilizer.
+ *
+ * VectionVR Stabilizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VectionVR Stabilizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VectionVR Stabilizer.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.vectionvr.jort.net;
 
 import com.vectionvr.jort.data.SensorData;
@@ -7,8 +25,7 @@ import static java.nio.ByteBuffer.allocate;
 import java.nio.ByteOrder;
 
 /**
- *
- * @author nico
+ * @author (Nicolas Chalon) n.chalon@bnome.be
  */
 class BinaryPacketConverter implements PacketConverter{
 
